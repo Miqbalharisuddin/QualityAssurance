8fdf60d366bbd481978cbd00dba6e251316d4eff
Miqbalharisuddin <miqbalharisuddin99@gmail.com>
2023-01-27 07:09:34 +0700

    update directory

---
+++ Automation Testing/Selenium Python/Testing Web OrangeHRM/test_menuMyInfo.py
@@ -0,0 +1,49 @@
+import unittest
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+class TestMenuMyInfo(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Chrome(ChromeDriverManager().install())
+
+    def test_menuMyInfo(self):
+        browser = self.browser
+        # buka situs
+        browser.get(
+            "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+        browser.maximize_window()
+        time.sleep(5)
+        # isi username
+        browser.find_element(
+            By.XPATH, "/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[1]/div/div[2]/input").send_keys("Admin")
+        time.sleep(1)
+        # isi password
+        browser.find_element(
+            By.XPATH, "//*[@id='app']/div[1]/div/div[1]/div/div[2]/div[2]/form/div[2]/div/div[2]/input").send_keys("admin123")
+        time.sleep(1)
+        # klik buton login
+        browser.find_element(
+            By.XPATH, "/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button").click()
+        time.sleep(8)
+        # klik menu admin
+        browser.find_element(
+            By.XPATH, "/html/body/div/div[1]/div[1]/aside/nav/div[2]/ul/li[6]/a").click()
+        time.sleep(8)
+        # validasi
+        expectedURL = "https://opensource-demo.orangehrmlive.com/web/index.php/pim/viewPersonalDetails/empNumber/7"
+        actualURL = browser.current_url
+        self.assertEquals(expectedURL, actualURL)
+
+    def tearDown(self):
+        self.browser.close()
+
+
+if __name__ == "__main__":
+    unittest.main()
