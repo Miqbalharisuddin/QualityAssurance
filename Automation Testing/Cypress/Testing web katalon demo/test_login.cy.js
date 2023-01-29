3d7a01c18a9a9a27899f9d50be954f9e91cc20ab
M Iqbal Harisuddin <66994713+Miqbalharisuddin@users.noreply.github.com>
2023-01-29 08:26:36 +0700

    Update test_login.cy.js

--- Automation Testing/Cypress/Testing web katalon demo/test_login.cy.js
+++ Automation Testing/Cypress/Testing web katalon demo/test_login.cy.js
@@ -14,8 +14,40 @@ describe('Login', () => {
         cy.get('#menu-toggle > .fa').click()
         cy.get('.sidebar-nav > :nth-child(4) > a').click()
         cy.get('#txt-username').type('Yanto')
+        cy.get('#txt-password').type('ThisIsNotAPassword')
+        cy.get('#btn-login').click()
+        cy.get('.text-danger')
+
+    })
+
+    it('Failed Login Invalid Password', () => {
+        cy.visit('https://katalon-demo-cura.herokuapp.com/')
+        cy.get('#menu-toggle > .fa').click()
+        cy.get('.sidebar-nav > :nth-child(4) > a').click()
+        cy.get('#txt-username').type('John Doe')
         cy.get('#txt-password').type('Yantonal123')
         cy.get('#btn-login').click()
+
+    })
+    it('Failed Login Invalid Username & Password', () => {
+        cy.visit('https://katalon-demo-cura.herokuapp.com/')
+        cy.get('#menu-toggle > .fa').click()
+        cy.get('.sidebar-nav > :nth-child(4) > a').click()
+        cy.get('#txt-username').type('Yanto')
+        cy.get('#txt-password').type('Yantonal123')
+        cy.get('#btn-login').click()
+        cy.get('.text-danger')
+
+    })
+
+    it('Failed Login Blank Field', () => {
+        cy.visit('https://katalon-demo-cura.herokuapp.com/')
+        cy.get('#menu-toggle > .fa').click()
+        cy.get('.sidebar-nav > :nth-child(4) > a').click()
+        cy.get('#txt-username').type(' ')
+        cy.get('#txt-password').type(' ')
+        cy.get('#btn-login').click()
+        cy.get('.text-danger')
 
     })
 })
