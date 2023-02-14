2be3e2d946bbace44cfcbee4a4787c40016c75be
Miqbalharisuddin <miqbalharisuddin99@gmail.com>
2023-02-14 19:53:02 +0700

    update directory

--- Lelang/Register.py
+++ Lelang/Register.py
@@ -28,36 +28,44 @@ class TestRegister(unittest.TestCase):
         browser.find_element(By.ID, "alamat_id").send_keys("Jombang")
         time.sleep(1)
         # klik dropdown provinsi
-        browser.find_element(By.XPATH, "/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/i").click()
+        browser.find_element(
+            By.XPATH, "/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/i").click()
         time.sleep(2)
         # pilih item dropdown provinsi
-        browser.find_element(By.XPATH, "/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/div[1]").click()
+        browser.find_element(
+            By.XPATH, "/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/div[1]").click()
         time.sleep(2)
         # klik dropdown kabupaten
-        browser.find_element(By.XPATH, "/html/body/div/div[1]/div/div[1]/div/div[2]/i").click()
+        browser.find_element(
+            By.XPATH, "/html/body/div/div[1]/div/div[1]/div/div[2]/i").click()
         time.sleep(2)
         # pilih item dropdown kabupaten
-        browser.find_element(By.XPATH, "/html/body/div/div[1]/div/div[1]/div/div[2]").click()
+        browser.find_element(
+            By.XPATH, "/html/body/div/div[1]/div/div[1]/div/div[2]").click()
         time.sleep(2)
         # isi kode pos
         browser.find_element(By.ID, "kodepos_id").send_keys("61456")
         time.sleep(1)
         # klik dropdown kartu identitas
-        browser.find_element(By.XPATH, "/html/body/div/div[1]/div/div[1]/div/div[2]/i").click()
+        browser.find_element(
+            By.XPATH, "/html/body/div/div[1]/div/div[1]/div/div[2]/i").click()
         time.sleep(2)
         # pilih item dropdown identitas
-        browser.find_element(By.XPATH, "/html/body/div/div[1]/div/div[1]/div/div[2]").click()
+        browser.find_element(
+            By.XPATH, "/html/body/div/div[1]/div/div[1]/div/div[2]").click()
         time.sleep(2)
         # isi nomer identitas
         browser.find_element(By.ID, "notas_id").send_keys("345671891030")
         time.sleep(1)
         # klik Choose file identitas
-        browser.find_element(By.XPATH, "/html/body/div/div[1]/div/div[1]").click()
+        browser.find_element(
+            By.XPATH, "/html/body/div/div[1]/div/div[1]").click()
         self.choose_file("#file-upload", file_path)
         file_path = './downloads/ktp.jpg'
         time.sleep(2)
         # klik choose NPWP
-        rowser.find_element(By.XPATH, "/html/body/div/div[1]/div/div[1]").click()
+        rowser.find_element(
+            By.XPATH, "/html/body/div/div[1]/div/div[1]").click()
         self.choose_file("#file-upload", file_path)
         file_path = './downloads/npwp.jpg'
         time.sleep(2)
@@ -65,16 +73,18 @@ class TestRegister(unittest.TestCase):
         browser.find_element(By.ID, "notelp_id").send_keys("08678923456")
         time.sleep(1)
         # klik dropdown kartu debit
-        browser.find_element(By.XPATH, "/html/body/div/div[1]/div/div[1]/div/div[2]/i").click()
+        browser.find_element(
+            By.XPATH, "/html/body/div/div[1]/div/div[1]/div/div[2]/i").click()
         time.sleep(2)
         # pilih item dropdown kartu debit
-        browser.find_element(By.XPATH, "/html/body/div/div[1]/div/div[1]/div/div[2]").click()
+        browser.find_element(
+            By.XPATH, "/html/body/div/div[1]/div/div[1]/div/div[2]").click()
         time.sleep(2)
         # isi No Rekening
-        browser.find_element(By.ID, "norekening_id").send_keys("33333678678923456")
+        browser.find_element(By.ID, "norek_id").send_keys("33333678678923456")
         time.sleep(1)
         # isi Nama Rekening
-        browser.find_element(By.ID, "rekening_id").send_keys("M Iqbal Harisuddin")
+        browser.find_element(By.ID, "rek_id").send_keys("M Iqbal Harisuddin")
         time.sleep(1)
         # isi Password
         browser.find_element(By.ID, "pass_id").send_keys("iqbal123")
@@ -83,13 +93,16 @@ class TestRegister(unittest.TestCase):
         browser.find_element(By.ID, "repeatpass_id").send_keys("iqbal123")
         time.sleep(1)
         # klik cekbox bukan robot
-        browser.find_element(By.XPATH, "/html/body/div/div[1]/div/div[1]/div/div[2]").click()
+        browser.find_element(
+            By.XPATH, "/html/body/div/div[1]/div/div[1]/div/div[2]").click()
         time.sleep(2)
         # klik pernyataan benar
-        browser.find_element(By.XPATH, "/html/body/div/div[1]/div/div[1]/div/div[3]").click()
+        browser.find_element(
+            By.XPATH, "/html/body/div/div[1]/div/div[1]/div/div[3]").click()
         time.sleep(2)
         # klik menyetujui ketentuan
-        browser.find_element(By.XPATH, "/html/body/div/div[1]/div/div[1]/div/div[4]").click()
+        browser.find_element(
+            By.XPATH, "/html/body/div/div[1]/div/div[1]/div/div[4]").click()
         time.sleep(2)
         # klik tombol Daftar
         browser.find_element(By.ID, "daftar_btn").click()
